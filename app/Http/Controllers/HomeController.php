98586c5e23e335cc6be361b3e75f8cb02c5f258e
farabi1233 <hosnain15-1233@diu.edu.bd>
2021-06-18 23:32:57 +0600

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -15,6 +16,11 @@ class HomeController extends Controller
     
     public function index()
     {
-        return view('backend.layouts.home');
+        $data['all_admins'] = User::where('usertype','Admin')->get()->count();
+        $data['all_users'] = User::get()->count();
+      
+        $data['all_students'] = User::where('usertype','Student')->get()->count();
+        $data['all_employees'] = User::where('usertype','Employee')->get()->count();
+        return view('backend.layouts.home',$data);
     }
 }
--- resources/views/backend/layouts/home.blade.php
+++ resources/views/backend/layouts/home.blade.php
@@ -1,137 +1,233 @@
-@extends('backend/layouts/master')
+@extends('backend.layouts.master')
 @section('content')
-<div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0 text-dark">Dashboard</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard v1</li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <!-- Small boxes (Stat box) -->
-                    <div class="row">
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>150</h3>
-
-                                    <p>New Orders</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-bag"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                                    <p>Bounce Rate</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>44</h3>
-
-                                    <p>User Registrations</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>65</h3>
-
-                                    <p>Unique Visitors</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                    </div>
-                    <!-- /.row -->
-                    <!-- Main row -->
-                    <div class="row">
-                        <!-- Left col -->
-                        <section class="col-lg-7 connectedSortable">
-                            <!-- Custom tabs (Charts with tabs)-->
-                            <div class="card">
-                                <div class="card-header">
-                                    <h3 class="card-title">
-                                        <i class="fas fa-chart-pie mr-1"></i>
-                                        Sales
-                                    </h3>
-                                    <div class="card-tools">
-                                        <ul class="nav nav-pills ml-auto">
-                                            <li class="nav-item">
-                                                <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div><!-- /.card-header -->
-                                <div class="card-body">
-                                    <div class="tab-content p-0">
-                                        <!-- Morris chart - Sales -->
-                                        <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
-                                            <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
-                                        </div>
-                                        <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
-                                            <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
-                                        </div>
-                                    </div>
-                                </div><!-- /.card-body -->
-                            </div>
-                            <!-- /.card -->
-
-
-                        </section>
-                        <!-- /.Left col -->
-                        <!-- right col (We are only adding the ID to make the widgets sortable)-->
-
-                        <!-- right col -->
-                    </div>
-                    <!-- /.row (main row) -->
-                </div><!-- /.container-fluid -->
-            </section>
-            <!-- /.content -->
+  <div class="content-wrapper">
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div>
         </div>
+      </div>
+    </div>
+   
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-info">
+                  <div class="inner">
+                    <h3>{{ $all_users }}</h3>
+                    <p>Total Users</p>
+                  </div>
+                  <div class="icon">
+                    <i class="fas fa-users"></i>
+                  </div>
+                  <a href="{{ route('users.view') }}" class="small-box-footer" target="_blank">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>{{ $all_admins }}</h3>
+                <p>Total Admins</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-shield"></i>
+              </div>
+              <a href="{{ route('users.view') }}" class="small-box-footer" target="_blank">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{ $all_employees }}</h3>
+                <p>Total Employees</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-tie"></i>
+              </div>
+              <a href="{{ route('employees.registration.view') }}" class="small-box-footer"  target="_blank">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>{{ $all_students }}</h3>
+
+                <p>Total Students</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-graduate"></i>
+              </div>
+              <a href="{{ route('students.registration.view') }}" class="small-box-footer"  target="_blank">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+         
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3><i class="fas fa-link"></i></h3>
+                <p>Monthly/Yearly Profit</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-hand-holding-usd"></i>
+              </div>
+              {{-- <a href="{{ route('reports.profit.view') }}" class="small-box-footer" target="_blank">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3><i class="fas fa-link"></i></h3>
+                <p>Marksheet Generate</p>
+              </div>
+              <div class="icon">
+                <i class="far fa-file-alt"></i>
+              </div>
+              {{-- <a href="{{ route('reports.marksheet.view') }}" class="small-box-footer"  target="_blank">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3><i class="fas fa-link"></i></h3>
+
+                <p>Attendance Report</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-file-signature"></i>
+              </div>
+              {{-- <a href="{{ route('reports.attendance.view') }}" class="small-box-footer"  target="_blank">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3><i class="fas fa-link"></i></h3>
+                <p>Student ID Card</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-id-card"></i>
+              </div>
+              {{-- <a href="{{ route('reports.id_card.view') }}" class="small-box-footer" target="_blank">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box shadow-lg">
+              <span class="info-box-icon bg-danger"><i class="fas fa-user-check"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Student Registration</span>
+                <a href="{{ route('students.registration.add') }}" class="small-box-footer" target="_blank">Click here <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box shadow-lg">
+              <span class="info-box-icon bg-danger"><i class="fas fa-chalkboard-teacher"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Employee Registration</span>
+                <a href="{{ route('employees.registration.add') }}" class="small-box-footer" target="_blank">Click here <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box shadow-lg">
+              <span class="info-box-icon bg-danger"><i class="fas fa-file-signature"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Marks Entry</span>
+                <a href="{{ route('marks.add') }}" class="small-box-footer" target="_blank">Click here <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box shadow-lg">
+              <span class="info-box-icon bg-danger"><i class="fas fa-file-contract"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Employee Attendence</span>
+                <a href="{{ route('employees.attendance.add') }}" class="small-box-footer" target="_blank">Click here <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 col-12">
+              <div class="info-box shadow-lg">
+                <span class="info-box-icon bg-danger"><i class="fas fa-user-check"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Student Registration</span>
+                  <a href="{{ route('students.registration.add') }}" class="small-box-footer" target="_blank">Click here <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-12">
+              <div class="info-box shadow-lg">
+                <span class="info-box-icon bg-danger"><i class="fas fa-chalkboard-teacher"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Employee Registration</span>
+                  <a href="{{ route('employees.registration.add') }}" class="small-box-footer" target="_blank">Click here <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-12">
+              <div class="info-box shadow-lg">
+                <span class="info-box-icon bg-danger"><i class="fas fa-file-signature"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Marks Entry</span>
+                  <a href="{{ route('marks.add') }}" class="small-box-footer" target="_blank">Click here <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-12">
+              <div class="info-box shadow-lg">
+                <span class="info-box-icon bg-danger"><i class="fas fa-file-contract"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Employee Attendence</span>
+                  <a href="{{ route('employees.attendance.add') }}" class="small-box-footer" target="_blank">Click here <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-12 connectedSortable">
+            <!-- Custom tabs (Charts with tabs)-->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">
+                  <i class="fas fa-chart-pie mr-1"></i>
+                  Activity
+                </h3>
+                <div class="card-tools">
+                 
+                </div>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content p-0">
+                  <!-- Morris chart - Sales -->
+                  <h6>No activity found!</h6>
+                </div>
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </section>
+          <!-- /.Left col -->
+        </div>
+      </div>
+    </section>
+  </div>
 @endsection
